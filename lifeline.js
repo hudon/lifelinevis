95c2e3db864f95553de04c7c512c154f08bce334
James Hudon <jameshudon@gmail.com>
2012-10-03 09:05:32 -0400

    init git, add ll node data structure

---
+++ lifeline.js
@@ -0,0 +1,14 @@
+var node = {}
+var lifeline = [new Node(0,1,1,1,10), new Node(2,0,1,1,12)]
+
+function Node(pid, tid, pidSource, tidSource, time) {
+  if (!(this instanceof Node)) {
+    return new Node();
+  }
+
+  this.srcProcessId = pidSource;
+  this.srcThreadId = tidSource;
+  this.processId = pid;
+  this.threadId = tid;
+  this.time = time;
+}
