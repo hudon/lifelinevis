155a5ee5d10ce441f4060815e3255fd0bede02c6
James Hudon <jameshudon@gmail.com>
2012-10-10 08:21:28 -0400

    refactor to pass jslint.com

--- lifeline.js
+++ lifeline.js
@@ -1,5 +1,6 @@
 (function () {
     'use strict';
+    /*jslint browser: true*/
 
     function Node(pid, tid, pidSource, tidSource, time) {
         if (!(this instanceof Node)) {
@@ -13,14 +14,7 @@
     }
 
     //var node = {}, lifeline = [new Node(0, 1, 1, 1, 10), new Node(2, 0, 1, 1, 12)];
-    var lifeline = [];
-
-    window.addEventListener('load', windowLoadHandler, false);
-
-    function windowLoadHandler() {
-        loadLifeline();
-        redrawLifelineUI();
-    }
+    var lifeline = [], util = {};
 
     function loadLifeline() {
         util.ajaxget('/lifeline.json', function (response) {
@@ -30,9 +24,9 @@
 
     function redrawLifelineUI() {
         var node, container, i,
-                div = document.getElementById('lifeline');
+            div = document.getElementById('lifeline');
 
-        for (i = 0; i < lifeline.length; i++) {
+        for (i = 0; i < lifeline.length; i += 1) {
             node = lifeline[i];
 
             // create node UI
@@ -44,15 +38,20 @@
         }
     }
 
-    /* Utilities */
+    function windowLoadHandler() {
+        loadLifeline();
+        redrawLifelineUI();
+    }
 
-    var util = util ? util : new Object();
+    window.addEventListener('load', windowLoadHandler, false);
+
+    /* Utilities */
 
     util.ajaxget = function (url, callback, async) {
         var r = util.getAjaxRequest(callback);
         r.open("GET", url, async);
         r.send(null);
-    }
+    };
 
     util.getAjaxRequest = function (callback) {
         var ajaxRequest;
@@ -60,27 +59,27 @@
         try {
             ajaxRequest = new XMLHttpRequest();
         } catch (e) {
+            /*global ActiveXObject*/
             try {
                 ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch (e) {
+            } catch (e2) {
                 try {
                     ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e){
+                } catch (e3) {
                     return null;
                 }
             }
         }
 
-        ajaxRequest.onreadystatechange = function() {
-            if (ajaxRequest.readyState == 4) {
-                // Prob want to do some error or response checking, but for 
-                // this example just pass the responseText to our callback function
+        ajaxRequest.onreadystatechange = function () {
+            if (ajaxRequest.readyState === 4) {
+                // TODO error response checking
                 callback(ajaxRequest.responseText);
             }
         };
 
         return ajaxRequest;
-    }
+    };
 
 }());
 
