6b2dca6506fa3ab08d9832cc11eded730eb67a74
James Hudon <jameshudon@gmail.com>
2012-10-10 07:46:51 -0400

    add basic structure for loading json and starting UI

--- lifeline.js
+++ lifeline.js
@@ -1,19 +1,86 @@
 (function () {
     'use strict';
 
-
     function Node(pid, tid, pidSource, tidSource, time) {
         if (!(this instanceof Node)) {
             return new Node();
         }
         this.srcProcessId = pidSource;
         this.srcThreadId = tidSource;
-        this.processId = pid;
-        this.threadId = tid;
+        this.dstProcessId = pid;
+        this.dstThreadId = tid;
         this.time = time;
     }
 
-    var node = {}, lifeline = [new Node(0, 1, 1, 1, 10), new Node(2, 0, 1, 1, 12)];
+    //var node = {}, lifeline = [new Node(0, 1, 1, 1, 10), new Node(2, 0, 1, 1, 12)];
+    var lifeline = [];
+
+    window.addEventListener('load', windowLoadHandler, false);
+
+    function windowLoadHandler() {
+        loadLifeline();
+        redrawLifelineUI();
+    }
+
+    function loadLifeline() {
+        util.ajaxget('/lifeline.json', function (response) {
+            lifeline = JSON.parse(response);
+        }, false);
+    }
+
+    function redrawLifelineUI() {
+        var node, container, i,
+                div = document.getElementById('lifeline');
+
+        for (i = 0; i < lifeline.length; i++) {
+            node = lifeline[i];
+
+            // create node UI
+            container = document.createElement('p');
+            container.appendChild(document.createTextNode(JSON.stringify(node)));
+
+            div.appendChild(container);
+
+        }
+    }
+
+    /* Utilities */
+
+    var util = util ? util : new Object();
+
+    util.ajaxget = function (url, callback, async) {
+        var r = util.getAjaxRequest(callback);
+        r.open("GET", url, async);
+        r.send(null);
+    }
+
+    util.getAjaxRequest = function (callback) {
+        var ajaxRequest;
+
+        try {
+            ajaxRequest = new XMLHttpRequest();
+        } catch (e) {
+            try {
+                ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e){
+                    return null;
+                }
+            }
+        }
+
+        ajaxRequest.onreadystatechange = function() {
+            if (ajaxRequest.readyState == 4) {
+                // Prob want to do some error or response checking, but for 
+                // this example just pass the responseText to our callback function
+                callback(ajaxRequest.responseText);
+            }
+        };
+
+        return ajaxRequest;
+    }
 
 }());
 
