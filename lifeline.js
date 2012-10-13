b4a665581f38248f26e687fb8147e2394e3fa489
Greta Cutulenco <gcutulenco@gmail.com>
2012-10-12 23:39:46 -0400

    test required tree json, add d3 tree code

--- lifeline.js
+++ lifeline.js
@@ -60,7 +60,7 @@
             if (p_bucket_node.treeNum) {
                 var tree = treeLifeline[p_bucket_node.treeNum];                 // a root Node 
                 addTreeChild(tree, 0, level, p_bucket_node.vertex, vert);       
-                addToBucket(level+1, vert, tree_loc);                           // add child to the right bucket
+                addToBucket(level+1, vert, p_bucket_node.treeNum);              // add child to the right bucket
             
             // do not have a parent in our current trees, create a new root == level 0
             } else {
@@ -115,27 +115,134 @@
         var bNode = new BucketNode(vertex, treeNum);
         buckets[level].push(bNode);
     }
+    
+    var duration = 500,
+        i = 0,
+        root,
+        tree,
+        diagonal,
+        vis;
+    
+    function update(source) {
+        var nodes = tree.nodes(root).reverse();
+        console.log(nodes);
+        var node = vis.selectAll("g.node")
+            .data(nodes, function(d) { return d.id || (d.id = ++i); });
+            
+        var nodeEnter = node.enter().append("svg:g")
+    	    .attr("class", "node");
+    	
+	    nodeEnter.append("svg:circle")
+            .attr("r", 4.5)
+            .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; })
+            .on("click", click);
+            
+        nodeEnter.append("svg:text")
+      	    .attr("x", function(d) { return d._children ? -8 : 8; })
+		    .attr("y", 3)
+      	    .text(function(d) { 
+      	        return "process : " + d.pid + " thread : " + d.tid; 
+      	    });
+    
+        // Transition nodes to their new position.
+	    nodeEnter.transition()
+		    .duration(duration)
+		    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+          	.style("opacity", 1)
+          .select("circle")
+            .style("fill", "lightsteelblue");
+          
+        node.transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+          .style("opacity", 1);
+        
+
+	    node.exit().transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+          .style("opacity", 1e-6)
+          .remove();
+          
+        // Update the links…
+        var link = vis.selectAll("path.link")
+            .data(tree.links(nodes), function(d) { return d.target.id; });
+
+        // Enter any new links at the parent's previous position.
+        link.enter().insert("svg:path", "g")
+            .attr("class", "link")
+            .attr("d", function(d) {
+                var o = {x: source.x0, y: source.y0};
+                return diagonal({source: o, target: o});
+            })
+            .transition()
+                .duration(duration)
+                .attr("d", diagonal);
+
+        // Transition links to their new position.
+        link.transition()
+            .duration(duration)
+            .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition()
+            .duration(duration)
+            .attr("d", function(d) {
+                var o = {x: source.x, y: source.y};
+                return diagonal({source: o, target: o});
+            })
+            .remove();
+
+        // Stash the old positions for transition.
+        nodes.forEach(function(d) {
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+    }
+
+    // Toggle children on click.
+    function click(d) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
+        }
+        update(d);
+    }
 
     function redrawLifelineUI() {
-        var node, container, i,
-            div = document.getElementById('lifeline');
-        /*for (i = 0; i < lifeline.length; i += 1) {
+        var w = 960,
+            h = 2000;
+            
+        tree = d3.layout.tree()
+            .size([h, w - 160]);
 
-            node = lifeline[i];
+        diagonal = d3.svg.diagonal()
+            .projection(function(d) { return [d.y, d.x]; });
 
-            // create node UI
-            container = document.createElement('p');
-            container.appendChild(document.createTextNode(JSON.stringify(node)));
-
-            div.appendChild(container);
-
-        }*/
+        vis = d3.select("#lifeline").append("svg:svg")
+            .attr("width", w)
+            .attr("height", h)
+          .append("svg:g")
+            .attr("transform", "translate(40,0)");
+          
+        // want to draw from treeLifeline structure    
+        d3.json("tree_example.json", function(json) {
+            json.x0 = 800;
+            json.y0 = 0;
+            root = json;
+            update(json);
+        });
+        
+        //update(treeLifeline);
     }
 
     function windowLoadHandler() {
         loadLifeline();
         parseLifelineData();
-        //redrawLifelineUI();
+        redrawLifelineUI();
     }
 
     window.addEventListener('load', windowLoadHandler, false);
