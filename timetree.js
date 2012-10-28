110d9dc7ab3ee3c00c5dd6624c26c41c295304ef
James Hudon <jameshudon@gmail.com>
2012-10-28 10:58:40 -0400

    fix tree building, refactor, start drawing time scale at bottom (currently broken)

--- timetree.js
+++ timetree.js
@@ -5,78 +5,73 @@ var TimeTree = (function () {
 
     // nodes per level
     var buckets = [],
+        nodeIdentifier = 0,
         timePeriod = 100;
 
     // Vertex: a process thread; Edge: the event of sending/receiving a tag
-    function Vertex(pid, tid, time) {
+    function Vertex(pid, tid, time, realtime) {
         this.pid = pid;
         this.tid = tid;
         this.time = time;
+        this.realtime = realtime;
     }
 
     function Node(vertex, children) {
+        children = children || [];
         this.pid = vertex.pid;
         this.tid = vertex.tid;
+        // 'time' might be scaled later for visualization
         this.time = vertex.time;
+        // 'realtime' is never scaled
+        this.realtime = vertex.realtime;
         this.children = children;
 
-        this.addChild = function addChild(childVertex) {
-            var childNode = new Node(childVertex, []);
+        this.bucketLevel = 0;
+
+        this.addChild = function addChild(childVertex, grandchildren) {
+            var childNode = new Node(childVertex, grandchildren);
             children.push(childNode);
+            return childNode;
         };
     }
 
-    function BucketNode(vertex, treeNum) {
-        this.vertex = vertex;
-        this.treeNum = treeNum;
-    }
-
-    // return tree, time
     // finding a parent - check all vertices on the level
     function findParent(vertex, bucket) {
         return _.find(bucket, function (bNode) {
-            return vertex.pid === bNode.vertex.pid && vertex.tid === bNode.vertex.tid;
+            return vertex.pid === bNode.pid && vertex.tid === bNode.tid;
         });
     }
 
-    function addTreeChildHelper(node, currLevel, goalLevel, parentVertex, childVertex) {
-        if (currLevel === goalLevel) {
-            if (node.pid === parentVertex.pid
-                    && node.tid === parentVertex.tid
-                    && node.time === parentVertex.time) {
-                node.addChild(childVertex);
-            }
-        } else {
-            _.each(node.children, function (child) {
-                addTreeChildHelper(child, currLevel + 1, goalLevel, parentVertex, childVertex);
-            });
-        }
-    }
-
-    function addTreeChild(tree, parentLevel, parentVertex, childVertex) {
-        addTreeChildHelper(tree, 0, parentLevel, parentVertex, childVertex);
-    }
-
-    function addToBucket(level, vertex, treeNum) {
-        var bucket,
-            bNode;
-
+    function addToBucket(level, node) {
         buckets[level] = buckets[level] || [];
-        bucket = buckets[level];
+        buckets[level].push(node);
+    }
 
-        // check if similar node is already in the bucket
-        bNode = _.find(bucket, function (bucketNode) {
-            return vertex.pid === bucketNode.vertex.pid && vertex.tid === bucketNode.vertex.tid;
-        });
+    function drawTimeline(width, graphHeight, graphSVG) {
+        var line, lineVis, lineData, level;
 
-        if (!bNode) {
-            bNode = new BucketNode(vertex, treeNum);
-        } else {
-            bNode.treeNum = treeNum;
-            bNode.vertex.time = vertex.time;
+        lineData = [];
+        for (level = 0; level < buckets.length; level += 1) {
+            lineData.push(timePeriod * level * 1.3 + 150);
         }
 
-        buckets[level].push(bNode);
+        lineVis = d3.select("#lifeline")
+            .append("svg:svg")
+            .attr("width", width)
+            .attr("height", 150);
+
+        line = d3.svg.line()
+            .x(function (d) { return d; })
+            .y(function (d) { return 0; });
+
+        lineVis.selectAll(".timeline")
+            .data(lineData)
+            .enter().append("svg:path")
+            .attr("d", line(lineData))
+            .attr("class", "timeline")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 3);
+
     }
 
     /**
@@ -98,34 +93,21 @@ var TimeTree = (function () {
         }
 
 
+        var nodes, nodeEnter, nodeUpdate, nodeExit, link, node;
+
         // result is an array of objs with x and y
         // locations (+vertex info)
-        var nodes = tree.nodes(root).reverse(),
-            nodeIdentifier,
-            // the enter set of node elements (i.e. all new nodes on screen)
-            nodeEnter,
-            nodeUpdate,
-            nodeExit,
-            link,
-            node;
+        nodes = tree.nodes(root).reverse();
 
-        // when we have more than 1 tree, needs more support in other parts of code
-        /*for (var i=0; i<source.length; i++) {
-            if (nodes) {
-                _.each(tree.nodes(source[i]).reverse(), function(node) { nodes.push(node) });
-            } else {
-                nodes = tree.nodes(source[i]).reverse();
+        nodes.forEach(function (d) {
+            if (d.parent) {
+                d.y = (d.bucketLevel * timePeriod + 100) * 1.3;
             }
-        }*/
-        //TODO didn't normalize
+        });
 
-        // creates as many g.node as vertices in tree
-        nodeIdentifier = 0;
-        // selects elements that don't exist in order to create
-        // new ones == empty selection
+        // data returns the "update selection" which contains nodes that exist
+        // in DOM and nodes that have not been added to DOM yet
         node = vis.selectAll("g.node")
-            // all nodes data ends up as placeholder nodes
-            // for missing elements in enter()
             .data(nodes, function (d) {
                 if (!d.id) {
                     nodeIdentifier += 1;
@@ -134,59 +116,51 @@ var TimeTree = (function () {
                 return d.id;
             });
 
-        /****** Dealing will all new node elements ******/
-
-        // for every node placeholder in enter set, add the missing elements to the SVG
-        // returns a list of SVG g elements (with node object in their data attrib)
+        // enter() gets the "enter selection" from the nodes returned by data().
+        // These nodes are not in the DOM yet.
         nodeEnter = node.enter()
             .append("svg:g")
             .attr("class", "node")
-            //TODO didn't transform
+            // this transform ensures the new nodes spawn from source
+            .attr("transform", function (d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
             .on("click", click);
 
-        // to each g elements add a SVG circle element (~~ as a child)
+        // to each new g elements add a SVG circle element, colour it if it
+        // has children
         nodeEnter.append("svg:circle")
             .attr("r", 1e-6)
             .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
 
-        // to each g element add a SVG text element (~~ another child)
+        // to each g element add a SVG text element
         nodeEnter.append("svg:text")
-            .attr("x", -5)//function(d) { return d._children ? -8 : 8; }) //--> used by original
+            .attr("x", -5)
             .attr("y", 18)
             .text(function (d) {
-                return "(pid : " + d.pid + ", tid : " + d.tid + ")";
+                if (!d.pid) { return ""; }
+                return "pid: " + d.pid + ", tid: " + d.tid + ", time: " + d.time + ")";
             });
 
         // Transition the g elements to their new position (duration controls
-        // speed: higher == slower)
+        // speed: higher == slower). This transitions all elements (existing
+        // and new)
         nodeUpdate = node.transition()
             .duration(animationDuration)
-             // translate nodes to x & y pos.
-            .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
-            /*.style("opacity", 1)
-            .select("circle")
-            .style("fill", "lightsteelblue");*/
+            .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
 
+        // Re-colour the nodes (since last colouring only affected new ones)
         nodeUpdate.select("circle")
             .attr("r", 4.5)
-            .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; })
+            .style("fill", function (d) { return d._children ? "lightsteelblue" : "#fff"; });
 
-        /****** done with new node elements ******/
-
-        // the update set (I believe)
-        nodeExit = node.transition()
-            .duration(animationDuration)
-            .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
-            .style("opacity", 1);
-
-        // The exit set -- remove any elements we don't need from screen
+        // The exit() set is all the elements that were not matched with
+        // data() above. We need to remove these.
         node.exit().transition()
             .duration(animationDuration)
             .attr("transform", function () { return "translate(" + source.y + "," + source.x + ")"; })
             .style("opacity", 1e-6)
             .remove();
 
-        /****** Vertex connection edges ******/
+        /****** Edges ******/
 
         // returns an array of objects representing the links from parent
         // to child for each node ({source, target})
@@ -218,7 +192,7 @@ var TimeTree = (function () {
             })
             .remove();
 
-        /****** done with link construction ******/
+        /****** done with edge construction ******/
 
         // Stash the old positions for transition.
         nodes.forEach(function (d) {
@@ -239,47 +213,49 @@ var TimeTree = (function () {
 
             // normalize the timestamps on the nodes
             _.each(lifeline, function (node) {
+                node.realtime = node.time;
                 node.time = Math.ceil(node.time / minTime) * 100;
             });
 
             _.each(lifeline, function (node) {
-                var vert = new Vertex(node.dstProcessId, node.dstThreadId, node.time),
-                    parent = new Vertex(node.srcProcessId, node.srcThreadId, node.time),
-                    parentBucketNode,
-                    // actual tree level based on time
-                    level = Math.floor(node.time / timePeriod),
+                // Vertices contain just the event data
+                var childVertex = new Vertex(node.dstProcessId, node.dstThreadId, node.time, node.realtime),
+                    parentVertex = new Vertex(node.srcProcessId, node.srcThreadId, node.time, node.realtime),
+                // Nodes contain tree-specific data (children, tree number,
+                // depth, etc.)
+                    childNode,
+                    parentNode,
+                // Levels represent tree depth (based off time)
+                    childLevel,
+                    parentLevel,
                     tree,
-                    rootNode,
                     bucket;
 
+                parentLevel = childLevel = Math.floor(node.time / timePeriod);
+
                 // find parent process: look through each level down to roots
-                while (level >= 0) {
-                    bucket = buckets[level] || [];
-                    parentBucketNode = findParent(parent, bucket);
-                    if (parentBucketNode) {
+                while (parentLevel > 0) {
+                    parentLevel -= 1;
+                    bucket = buckets[parentLevel] || [];
+                    parentNode = findParent(parentVertex, bucket);
+                    if (parentNode) {
                         break;
                     }
-                    level -= 1;
                 }
 
                 // have a parent, go to parent in identified tree and add the
                 // child -- update parent in bucket
-                if (parentBucketNode) {
-                    // tree is a root Node
-                    tree = treeLifeline[parentBucketNode.treeNum];
-                    addTreeChild(tree, level, parentBucketNode.vertex, vert);
-                    // add child to the right bucket
-                    addToBucket(level + 1, vert, parentBucketNode.treeNum);
-
-                } else {
+                if (!parentNode) {
                     // we do not have a parent in our current trees,
                     // create a new root at level 0
-                    rootNode = new Node(parent, []);
-                    rootNode.addChild(vert);
-                    treeLifeline.push(rootNode);
-                    addToBucket(0, parent, treeLifeline.length - 1);
-                    addToBucket(1, vert, treeLifeline.length - 1);
+                    parentNode = new Node(parentVertex, []);
+                    parentNode.time = 0;
+                    treeLifeline.push(parentNode);
+                    addToBucket(0, parentNode);
                 }
+                childNode = parentNode.addChild(childVertex);
+                childNode.bucketLevel = childLevel;
+                addToBucket(childLevel, childNode);
             });
 
             return treeLifeline;
@@ -291,31 +267,37 @@ var TimeTree = (function () {
             //d3.json("tree_example.json", function (json) {
             //_.each(json, function (jsonTree) {
 
-            _.each(treeLifeline, function (jsonTree) {
 
-                var w = 960, // the width and height of the whole svg arrea
-                    h = 700,//2000;
-                    tree = d3.layout.tree().size([h, w - 260]),
-                    animationDuration = 500,
-                    diagonal,
-                    vis;
+            var dummyNode,
+                w = 1760, // the width and height of the whole svg arrea
+                h = 800,//2000;
+                tree = d3.layout.tree().size([h, w - 760]),
+                animationDuration = 500,
+                diagonal,
+                vis;
 
-                diagonal = d3.svg.diagonal().projection(function (d) {
-                    return [d.y, d.x];
-                });
-
-                // creates an SVG canvas
-                vis = d3.select("#lifeline")
-                    .append("svg:svg")
-                    .attr("width", w)
-                    .attr("height", h)
-                    .append("svg:g")
-                    .attr("transform", "translate(20,0)"); //moves the initial position of the svg:g element
-
-                jsonTree.x0 = 300;//800;
-                jsonTree.y0 = 0;
-                update(jsonTree, jsonTree, diagonal, tree, animationDuration, vis);
+            dummyNode = new Node(new Vertex());
+            _.each(treeLifeline, function (tree) {
+                dummyNode.addChild(tree, tree.children);
             });
+
+            diagonal = d3.svg.diagonal().projection(function (d) {
+                return [d.y, d.x];
+            });
+
+            // creates an SVG canvas
+            vis = d3.select("#lifeline")
+                .append("svg:svg")
+                .attr("width", w)
+                .attr("height", h)
+                .append("svg:g")
+                .attr("transform", "translate(20,0)"); //moves the initial position of the svg:g element
+
+            dummyNode.x0 = 300;//800;
+            dummyNode.y0 = 0;
+            update(dummyNode, dummyNode, diagonal, tree, animationDuration, vis);
+
+            //drawTimeline(w, h, vis);
         }
     };
 }());
