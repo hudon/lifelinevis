6e4d4da0eba30d8000b49bea44a3a3a9cbf8f540
James Hudon <jameshudon@gmail.com>
2012-11-06 13:37:56 -0500

    remove utilities from lifeline

--- lifeline.js
+++ lifeline.js
@@ -21,40 +21,5 @@
 
     window.addEventListener('load', windowLoadHandler, false);
 
-    /* Utilities */
-    _.mixin({
-        ajaxget: function (url, callback, async) {
-            var r = _.getAjaxRequest(callback);
-            r.open("GET", url, async);
-            r.send(null);
-        },
-        getAjaxRequest: function (callback) {
-            var ajaxRequest;
-
-            try {
-                ajaxRequest = new XMLHttpRequest();
-            } catch (e) {
-                /*global ActiveXObject*/
-                try {
-                    ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-                } catch (e2) {
-                    try {
-                        ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                    } catch (e3) {
-                        return null;
-                    }
-                }
-            }
-
-            ajaxRequest.onreadystatechange = function () {
-                if (ajaxRequest.readyState === 4) {
-                    // TODO error response checking
-                    callback(ajaxRequest.responseText);
-                }
-            };
-
-            return ajaxRequest;
-        }
-    });
 }());
 
